01254a80f245aa4ddeb2f37130d16bcc391ed540
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-03 09:12:01 +0200

    Initial commit

---
+++ MapKitBasics/MapKitBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitBasicsApp.swift
+//  MapKitBasics
+//
+//  Created by Davide Castaldi on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
